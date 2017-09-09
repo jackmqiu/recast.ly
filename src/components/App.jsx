e06db62f968aebba9d2a11c348df71b1dca8143c
Hack Reactor Student <student@hackreactor.com>
2017-09-08 17:16:02 -0700

    Implement app, list, entry components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentlyPlaying: exampleVideoData[0]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentlyPlaying}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={exampleVideoData} handleVideoClick={(video) => this.setState({ currentlyPlaying})} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+  // handleVideoClick(video) {
+  //   console.log('clicked')
+  //   this.setState({ currentlyPlaying: video })
+  // }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
